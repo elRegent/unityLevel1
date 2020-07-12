d01dba6995797fe28c3168408716fa0fd15b3c45
elRegent <el.regent@gmail.com>
2020-07-12 21:23:05 +0300

    feat(hw2): Ассеты

---
+++ Assets/gameFirst/Scripts/GameScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    private GameObject _player;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        //player.transform.position += Vector3.up * 10;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/gameFirst/Scripts/HeroMoveScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMoveScript : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * _speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * _speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * _speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * _speed * Time.deltaTime;
+        }*/
+
+        float horizontal = Input.GetAxis("Horizontal");
+        if(horizontal != 0)
+        {
+            transform.position += transform.right * horizontal * _speed * Time.deltaTime;
+        }
+
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical != 0)
+        {
+            transform.position += transform.forward * vertical * _speed * Time.deltaTime;
+        }
+    }
+}
